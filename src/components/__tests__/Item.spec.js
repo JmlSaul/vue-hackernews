fb048910043051c21c8a649dc899c5f4360e0f6d
JmlSaul <JmlSaul@live.com>
2018-05-25 14:51:37 +0800

    chapter2 fin

---
+++ src/components/__tests__/Item.spec.js
@@ -0,0 +1,15 @@
+import {
+  shallowMount
+} from '@vue/test-utils'
+import Item from '../Item.vue'
+describe('Item.vue', () => {
+  test('renders item', () => {
+    const wrapper = shallowMount(Item)
+    debugger
+    expect(wrapper.text()).toContain('item')
+  })
+})
+
+// test('sanity test', () => {
+//   expect(true).toBe(true)
+// })
