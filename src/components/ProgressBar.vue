9f82d80ecb4482ff960d4893c09c3ae738825d22
Jmlsaul <jmlsaul@live.com>
2018-05-27 19:30:16 +0800

    chapter 4 done

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,13 +1,43 @@
 <template>
-  <div :style="{
-    'width': `${percent}%`
-  }" />
+  <div :class="{show:show,error:error}" :style="{'width': `${percent}%`}" />
 </template>
 
 <script>
 export default {
   data: () => ({
-    percent: 0
-  })
+    percent: 0,
+    show: false,
+    error: false,
+    _timer: null
+  }),
+  methods: {
+    start () {
+      this.show = true
+      this.percent = 0
+      this._timer = setInterval(() => {
+        this.percent++
+      }, 100)
+    },
+    finish () {
+      this.show = false
+      this.percent = 100
+      clearInterval(this._timer)
+      this._timer = null
+    },
+    fail () {
+      this.error = true
+      this.percent = 100
+    }
+  }
 }
 </script> 
+
+<style>
+.error {
+  color: red;
+}
+.show {
+  display: block;
+}
+</style>
+
--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,25 +1,36 @@
 <template>
   <div class="item-list-view">
     <div class="item-list">
-      <item
-        v-for="item in displayItems"
-        :key="item.id"
-        :item="item"
-      />
+      <item v-for="item in displayItems" :key="item.id" :item="item" />
     </div>
   </div>
 </template>
 
 <script>
 import Item from '../components/Item.vue'
+import { fetchListData } from '../api/api'
 
 export default {
   components: {
     Item
   },
+  beforeMount () {
+    this.loadItems()
+  },
   data () {
     return {
-      displayItems: window.items
+      displayItems: []
+    }
+  },
+  methods: {
+    loadItems () {
+      this.$bar.start()
+      fetchListData('top')
+        .then(items => {
+          this.displayItems = items
+          this.$bar.finish()
+        })
+        .catch(() => this.$bar.fail())
     }
   }
 }
@@ -27,19 +38,19 @@ export default {
 
 <style>
 .item-list-view {
-	padding-top: 45px;
+  padding-top: 45px;
 }
 .item-list {
-	background-color: #fff;
-	border-radius: 2px;
-	position: absolute;
-	margin: 30px 0;
-	width: 100%;
-	transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  background-color: #fff;
+  border-radius: 2px;
+  position: absolute;
+  margin: 30px 0;
+  width: 100%;
+  transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
 }
 @media (max-width: 600px) {
-	.item-list {
-		margin: 10px 0;
-	}
+  .item-list {
+    margin: 10px 0;
+  }
 }
 </style>
